b43aa113c5abf88eb955733c08f521e4bc0d2847
Andrew Pruitt <apruitt0321@gmail.com>
2019-06-21 16:07:47 -0500

    Removed unnecessary collects and iters.

--- lcdCalc.rs
+++ lcdCalc.rs
@@ -1,12 +1,14 @@
 use std::env;
 
 fn lcd(nums: &Vec<i32>, x: i32) -> i32 {
-    let v: Vec<bool> = nums.iter().map(|y| {
-        if x % y == 0 { 
-            true
-        } else {
-            false
-        }}).collect();
+    let v: Vec<bool> = nums.iter()
+        .map(|y| {
+            if x % y == 0 { 
+                true
+            } else {
+                false 
+            }
+        }).collect();
     if v.contains(&false) {
         lcd(nums, x+1)
     } else {
@@ -15,10 +17,10 @@ fn lcd(nums: &Vec<i32>, x: i32) -> i32 {
 }
 
 fn main() {
-    let a: Vec<String> = env::args().collect();
-    let args: Vec<i32> = a[1..].iter()
-        .map(|y| { y.parse::<i32>().unwrap() })
-        .collect();
+    let args: Vec<i32> = env::args().skip(1)
+        .map(|arg| {
+            arg.parse::<i32>().unwrap()
+        }).collect();
     let x = lcd(&args, 1);
     println!("The LCM of {:?} is {}", &args, &x);
 }
