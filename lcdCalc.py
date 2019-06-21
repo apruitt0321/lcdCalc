0bb94b13303cc5b5da3f3bf9a49ac1a5211a8c7e
Andrew Pruitt <apruitt0321@gmail.com>
2019-06-21 09:20:49 -0500

    Got rid of Counter object. Used lambda functions and map().

--- lcdCalc.py
+++ lcdCalc.py
@@ -1,27 +1,15 @@
-from collections import Counter
 import sys
 
 def calc_lcd(args):
     if len(args) < 2:
         return "Not enough arguments. Exiting..."
     else:
-        denoms = Counter()          # Counter object
-        count = 1                       # loop counter
+        i = 1
         while True:
-            for denominator in args:
-                denoms[denominator * count] += 1
-            # Takes most common product and the number of times it appears
-            product, num_common = denoms.most_common(1)[0]
-            # Checks if a product appears as many times as there are arguments
-            if num_common == len(args):
-                return f"Most common denominator of {args} is {product}"
-            else:
-                count += 1
-                # Prevents potential hanging. Could probably be removed.
-                if count == 200:
-                    return "Reached 200 iterations. Exiting..."
-        else:
-            return "Could not find a common denominator."
+            y = map(lambda x: True if i % x == 0 else False, args)
+            if False not in y:
+                return f"Least common denominator of {args} is {i}"
+            i += 1
 
 # Main
 if __name__ == "__main__":
