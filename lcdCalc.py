ff367381469c47f3a79fb7bdd05f1087e95be8f1
Andrew Pruitt <apruitt0321@gmail.com>
2019-06-21 00:34:13 -0500

    Added comments. Upped iteration limit to 200.

--- lcdCalc.py
+++ lcdCalc.py
@@ -5,23 +5,26 @@ def calc_lcd(args):
     if len(args) < 2:
         return "Not enough arguments. Exiting..."
     else:
-        denoms = Counter()
-        c = 1
+        denoms = Counter()          # Counter object
+        count = 1                       # loop counter
         while True:
-            for i in args:
-                denoms[i*c] += 1
-                most_common, num_common = denoms.most_common(1)[0]
+            for denominator in args:
+                denoms[denominator * count] += 1
+            # Takes most common product and the number of times it appears
+            product, num_common = denoms.most_common(1)[0]
+            # Checks if a product appears as many times as there are arguments
             if num_common == len(args):
-                return f"Most common denominator of {args} is {most_common}"
+                return f"Most common denominator of {args} is {product}"
             else:
-                c += 1
-                if c == 100:
-                    return "Reached 100 iterations. Exiting..."
+                count += 1
+                # Prevents potential hanging. Could probably be removed.
+                if count == 200:
+                    return "Reached 200 iterations. Exiting..."
         else:
             return "Could not find a common denominator."
 
+# Main
 if __name__ == "__main__":
     x = [int(i) for i in sys.argv[1:]]
     result = calc_lcd(x)
     print(result)
-
