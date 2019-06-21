19bfdd845279bf4419c2d6cf743d58d689cbf811
Andrew Pruitt <apruitt0321@gmail.com>
2019-06-21 12:39:54 -0500

    Added try/except. Moved IO to main.

--- lcdCalc.py
+++ lcdCalc.py
@@ -1,18 +1,16 @@
 import sys
 
 def calc_lcd(args):
-    if len(args) < 2:
-        return "Not enough arguments. Exiting..."
-    else:
-        i = 1
-        while True:
-            y = map(lambda x: True if i % x == 0 else False, args)
-            if False not in y:
-                return f"Least common multiple of {args} is {i}"
-            i += 1
+    i = max(args)
+    while True:
+        y = map(lambda x: True if i % x == 0 else False, args)
+        if False not in y: return i
+        i += 1
 
 # Main
 if __name__ == "__main__":
-    x = [int(i) for i in sys.argv[1:]]
-    result = calc_lcd(x)
-    print(result)
+    try:
+        x = [int(i) for i in sys.argv[1:]]
+        print(f"Least common multiple of {x} is {calc_lcd(x)}")
+    except:
+        print("Error. Check your arguments and try again.")
